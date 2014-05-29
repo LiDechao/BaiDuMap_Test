2a01b0f9a359c9887e2f47da90833d138b5ca04c
LiDechao <macbookpro@lidechaodeMacBook-Pro.local>
2014-05-29 18:32:14 +0800

    test
    test

---
+++ BaiDuMap_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaiDuMap_Test
+//
+//  Created by MacBook Pro on 14-4-23.
+//  Copyright (c) 2014年 MaFengWo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaiDuMap_TestTests/BaiDuMap_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  BaiDuMap_TestTests.m
+//  BaiDuMap_TestTests
+//
+//  Created by MacBook Pro on 14-4-23.
+//  Copyright (c) 2014年 MaFengWo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BaiDuMap_TestTests : XCTestCase
+
+@end
+
+@implementation BaiDuMap_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
